320b2464d036bf591893e7b479d39667c640ac37
shadman-shahid <shadman9085@gmail.com>
2022-12-14 19:03:55 +0600

    Commit before lfqa functionality.

--- test.py
+++ test.py
@@ -1,19 +1,28 @@
+# import os
+# import time
+#
+# from haystack.document_stores import ElasticsearchDocumentStore
+#
+# # Wait 30 seconds only to be sure Elasticsearch is ready before continuing
+# time.sleep(30)
+#
+# # Get the host where Elasticsearch is running, default to localhost
+# host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
+#
+# document_store = ElasticsearchDocumentStore(
+#     host=host,
+#     username="",
+#     password="",
+#     index='haystack-lfqa',
+#     similarity="cosine",
+#     embedding_dim=768
+# )
+
 import os
+from subprocess import Popen, PIPE, STDOUT
 import time
-
-from haystack.document_stores import ElasticsearchDocumentStore
-
-# Wait 30 seconds only to be sure Elasticsearch is ready before continuing
-time.sleep(30)
-
-# Get the host where Elasticsearch is running, default to localhost
-host = os.environ.get("ELASTICSEARCH_HOST", "localhost")
-
-document_store = ElasticsearchDocumentStore(
-    host=host,
-    username="",
-    password="",
-    index='haystack-lfqa',
-    similarity="cosine",
-    embedding_dim=768
+es_server = Popen(
+    ["elasticsearch-7.9.2/bin/elasticsearch"], stdout=PIPE, stderr=STDOUT, start_new_session=True  # as daemon
 )
+# wait until ES has started
+time.sleep(30)
